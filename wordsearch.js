660e483545d1fa6609e2fb0e64467c6cff0f8ccd
Samy0412 <samantha.gadet@gmail.com>
2020-06-03 22:07:20 +0000

    horizontal and vertical

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,51 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true; //As soon as it finds it
     }
-}
+  }
+  let verticalJoin = transpose(letters);
+  verticalJoin = verticalJoin.map((ls) => ls.join(""));
+  //console.log(verticalJoin);
+  for (l of verticalJoin) {
+    if (l.includes(word)) {
+      return true; //As soon as it finds it
+    }
+  }
 
-module.exports = wordSearch
+  return false; //AFTER it has checked vert and horiz
+};
+
+const transpose = function (matrix) {
+  let vertArray = [];
+  let newArrayCol = matrix[0].length;
+  for (let i = 0; i < newArrayCol; i++) {
+    vertArray.push([]);
+  }
+
+  for (let r = 0; r < matrix.length; r++) {
+    for (let c = 0; c < matrix[r].length; c++) {
+      vertArray[c].push(matrix[r][c]);
+    }
+  }
+
+  return vertArray;
+};
+
+/* wordSearch(
+  [
+    ["A", "W", "C", "F", "Q", "U", "A", "L"],
+    ["S", "E", "I", "N", "F", "E", "L", "D"],
+    ["Y", "F", "C", "F", "Q", "U", "A", "L"],
+    ["H", "M", "J", "T", "E", "V", "R", "G"],
+    ["W", "H", "C", "S", "Y", "E", "R", "L"],
+    ["B", "F", "R", "E", "N", "E", "Y", "B"],
+    ["U", "B", "T", "W", "A", "P", "A", "I"],
+    ["O", "D", "C", "A", "K", "U", "A", "S"],
+    ["E", "Z", "K", "F", "Q", "U", "A", "L"],
+  ],
+  "FRANK"
+); */
+
+module.exports = wordSearch;
